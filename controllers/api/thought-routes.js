df28051e5c3e9365e5ccdcda3a4e770488d5aeb9
Erik Plachta <eplachta@gmail.com>
2022-03-03 22:33:55 -0500

    Add controller/api framework. Add model index framework

---
+++ controllers/api/thought-routes.js
@@ -0,0 +1,10 @@
+//------------------------------------------------------------------------------
+//-- Imports
+const router = require('express').Router();
+const { Thought } = require('../../models');
+const withAuth = require('../../utils/auth.js')
+
+
+//------------------------------------------------------------------------------
+//-- Exports
+module.exports = router;
---
+++ models/index.js
@@ -0,0 +1,9 @@
+//-- Imports
+const User = require('./User');
+const Reaction = require('./Reaction');
+const Thought = require('./Thought');
+
+//-----------------------------------------------------------------------------
+//-- Exports
+
+module.exports = { User, Reaction, Thought };
