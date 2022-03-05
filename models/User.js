ebf22e650ab82fb6e9f0ec28b02789e7239e9a92
Erik Plachta <eplachta@gmail.com>
2022-03-05 17:22:23 -0500

    Add user model framework

--- models/User.js
+++ models/User.js
@@ -6,33 +6,26 @@ const UserSchema = new Schema(
         username: {
           type: String,
           trim: true,
-          required: 'Username is required',
+          unique: true,
+          required: 'ERROR: Username is required',
         },
-        firstName: {
-            type: String,
-            trim: true,
-            required: 'First Name is Required'
-          },
-      
-          lastName: {
-            type: String,
-            trim: true,
-            required: 'Last Name is Required'
-          },
-      
-          password: {
-            type: String,
-            trim: true,
-            required: 'Password is Required',
-            validate: [({ length }) => length >= 6, 'Password must be at least 6 characters.']
-          },
-      
-          email: {
-            type: String,
-            unique: true,
-            match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
-          },
-        userCreated: {
+        email: {
+          type: String,
+          unique: true,
+          required: 'ERROR: Email is required',
+          match: [/.+@.+\..+/, 'ERROR: Please enter a valid email address']
+        },
+        password: {
+          type: String,
+          trim: true,
+          required: 'ERROR: Password is required',
+          validate: [({ length }) => length >= 6, 'ERROR: Password must be at least 6 characters']
+        },
+        date_created: {
+          type: Date,
+          default: Date.now
+        },
+        date_login: {
           type: Date,
           default: Date.now
         }
@@ -44,7 +37,6 @@ const UserSchema = new Schema(
         id: false
       }
 );
-
 
 
 const User = model('User', UserSchema);
