727f34a653920b4db309a41b01509436c642b63c
Erik Plachta <eplachta@gmail.com>
2022-03-05 16:49:14 -0500

    Cleaned up seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -7,16 +7,13 @@
 */
 
 require('dotenv').config(); //-- for local variable caching
-const {mongoose, sequelize, db } = require('../config/connection')
+const { sequelize, db } = require('../config/connection')
 
 //------------------------------------------------------------------------------
-//-- Building seed database with mysql2 
-
+//-- Building seed database within MySQL Database
 async function seedDatabase () {
-
-  
-  // execute in parallel, next console.log in 3 seconds
   try {
+    // Execute in parallel. Once all compelted, move on.
     await Promise.all([
       db.query(`DROP DATABASE IF EXISTS ${process.env.DB_NAME}`),
       db.query('select sleep(4)'),
@@ -32,73 +29,31 @@ async function seedDatabase () {
     await db.end(); //-- Close connection
     return false; //-- return true as promise
   };
-
-}
+};
 
 //------------------------------------------------------------------------------
 //-- Building seed tables with Sequelize based on Model data and seed JSON data
 async function seedTables() {
-
-  // //-- Used to build SQL seed data, and erase anything that may exist
-  // const sequelize = require('../config/connection');
 
   //-- Grab database Table models
   const { WebUser } = require('../models');
 
   //-- Grab seed data to build a seed database
   const seed_Users = require('./seed_WebUser.json');
-  // const seed_Posts = require('./seed_Post.json');
-  // const seed_Comments = require('./seed_Comment.json');
-  // const seed_Resources = require('./seed_Resource.json');
-
+  
   //-- wait for connection to database
   await sequelize.sync({ force: true });
 
   // -- Grab all users and build Table based on Model
-  const users = await WebUser.bulkCreate(seed_Users, 
-    {
-      individualHooks: true,
-      returning: true,
-    }
+  const users = await WebUser.bulkCreate(
+    seed_Users, { individualHooks: true, returning: true,}
   );
   
   console.log(`\n//-- Created Users\n`)
   console.log(`//-----------------------------------------------------------\n`)
   
-  // //-- grab all KBAs and build Table based on Model
-  // for (const post of seed_Posts) {
-  //   const newPost = await Post.create({
-  //   ...post,
-  //   });
-  // }
-
-  // console.log(`\n//-- Created Posts\n`)
-  // console.log(`//-----------------------------------------------------------\n`)
-
-  //   // //-- grab all KBAs and build Table based on Model
-  //   for (const comment of seed_Comments) {
-  //     const newComment = await Comment.create({
-  //     ...comment,
-  //     });
-  //   }
-  
-  //   console.log(`\n//-- Created Posts\n`)
-  //   console.log(`//-----------------------------------------------------------\n`)
-
-  // //  //-- grab all Resources and build Table based on Model
-  //  for (const resource of seed_Resources) {
-  //   const newResource = await Resource.create({
-  //   ...resource,
-  //   });
-
-  //   console.log(`\n//-- Created Resources!\m`)
-  //   console.log(`//-----------------------------------------------------------\n`)
-  // }
-
-
   //-- exit once done building seed database data
-  // process.exit(0);
-  
+  process.exit(0);
 };
 
 
@@ -111,7 +66,7 @@ const seed = async () => {
     .then( results => console.log(`//-- database creation successful: ${results}`))
     .then(() => seedTables())
     .then( () => process.exit(0))
-    //-- print error
+    //-- print error if happens
     .catch(console.log)
 };
 
