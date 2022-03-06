f5ef1ee8106f4d5a40918241b5880a1508c326a1
Erik Plachta <eplachta@gmail.com>
2022-03-06 17:50:38 -0500

    Finalized removing Full-Stack content and onboarding headless

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 //-- onboarding Handlebars
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -20,14 +20,10 @@ mongoose.set('debug', true);
 
 app.use(routes);
 
-//-- Use existing tables if exist, start connection to express and SQL
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Now listening on http://127.0.0.1:${PORT}`));
-});
+// //-- Use existing tables if exist, start connection to express and SQL
+// sequelize.sync({ force: false }).then(() => {
+//   app.listen(PORT, () => console.log(`Now listening on http://127.0.0.1:${PORT}`));
+// });
 
 // Use this to log mongo queries being executed!
-mongoose.set('debug', true);
-
-app.use(require('./routes'));
-
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
